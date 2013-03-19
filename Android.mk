7fca8792ea5ca9380ab6b21cae269a2ee911fc83
Tobias Brunner <tobias@strongswan.org>
2013-03-19 18:24:33 +0100

    Only build libcrypto in the NDK

--- Android.mk
+++ Android.mk
@@ -10,5 +10,3 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/build-config-64.mk
 include $(LOCAL_PATH)/build-config-32.mk
 include $(LOCAL_PATH)/Crypto.mk
-include $(LOCAL_PATH)/Ssl.mk
-include $(LOCAL_PATH)/Apps.mk
